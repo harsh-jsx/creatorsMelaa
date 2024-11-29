98f1f461b251e32e2eb022b082b8455530f1605c
Maddy798 <hardiktaneja798@gmail.com>
2024-11-29 22:31:19 +0530

    added smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,31 @@ import Creators from "./Components/Creators/Creators";
 import Home from "./Pages/Home";
 import Footer from "./Components/Footer/Footer";
 import Offers from "./Components/WhatWeOffer/Offers";
+import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
 function App() {
   const [loading, setLoading] = useState(true);
 
+  const lenis = new Lenis();
+
+  lenis.on("scroll", (e) => {
+    console.log(e);
+  });
+
+  lenis.on("scroll", ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 500); // Convert time from seconds to milliseconds
+  });
+
+  // Disable lag smoothing in GSAP to prevent any delay in scroll animations
+  gsap.ticker.lagSmoothing(0);
+
   return (
     <div id="smooth-wrapper">
-      {loading && <Loader setLoading={setLoading} />}
+      {/* {loading && <Loader setLoading={setLoading} />} */}
       <div id="smooth-content">
         <Cursor />
         <Navbar />
