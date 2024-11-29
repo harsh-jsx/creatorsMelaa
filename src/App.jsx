61e07bf03430c9414cbdafb34623319ff5abb461
unknown <SCHW18TZ>
2024-11-30 02:49:50 +0530

    Made the website more optimised

--- src/App.jsx
+++ src/App.jsx
@@ -16,34 +16,33 @@ function App() {
   const [loading, setLoading] = useState(true);
   const progressBarRef = useRef(null);
 
-  const lenis = new Lenis();
-
-  lenis.on("scroll", (e) => {
-    console.log(e);
-  });
-
-  lenis.on("scroll", ScrollTrigger.update);
-
-  gsap.ticker.add((time) => {
-    lenis.raf(time * 500); // Convert time from seconds to milliseconds
-  });
-
-  // Disable lag smoothing in GSAP to prevent any delay in scroll animations
-  gsap.ticker.lagSmoothing(0);
-
   useEffect(() => {
+    const lenis = new Lenis();
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 500); // Convert time from seconds to milliseconds
+    });
+
+    // Disable lag smoothing in GSAP to prevent any delay in scroll animations
+    gsap.ticker.lagSmoothing(0);
+
     const handleScroll = () => {
       const scrollTop = window.scrollY;
       const docHeight =
         document.documentElement.scrollHeight - window.innerHeight;
       const scrollPercent = (scrollTop / docHeight) * 100;
-      progressBarRef.current.style.width = `${scrollPercent}%`;
+      if (progressBarRef.current) {
+        progressBarRef.current.style.width = `${scrollPercent}%`;
+      }
     };
 
     window.addEventListener("scroll", handleScroll);
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      gsap.ticker.remove(lenis.raf);
     };
   }, []);
 
