2323985734faf9d7e684dae916e2b6782890ce5c
unknown <SCHW18TZ>
2024-12-01 00:36:08 +0530

    Optimisations

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Home.css";
 import Creators from "../../Components/Creators/Creators";
 import Clients from "../../Components/OurClients/Clients";
 import Offers from "../../Components/WhatWeOffer/Offers";
 import Testimonials from "../../Components/Testimonials/Testimonials";
+import gsap from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+
+gsap.registerPlugin(TextPlugin);
 
 const Home = () => {
+  const texts = [
+    "Empowering Creators, Elevating Brands",
+    "Your Gateway to Influencer Success",
+    "Connect with Creators",
+  ];
+
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    let currentTextIndex = 0;
+    const typeText = () => {
+      const text = texts[currentTextIndex];
+      gsap.to(textRef.current, {
+        duration: 0.05 * text.length,
+        text: text,
+        ease: "none",
+        onComplete: () => {
+          currentTextIndex = (currentTextIndex + 1) % texts.length;
+          setTimeout(typeText, 2000); // Wait 2 seconds before typing the next text
+        },
+      });
+    };
+
+    typeText();
+  }, []);
+
   return (
     <>
       <div className="home">
@@ -21,7 +51,7 @@ const Home = () => {
             <span className="highlight">CreatorsMela</span>
           </h1>
           <h2 className="subtitle">Your Gateway to Influencer Success</h2>
-          <p className="description">Connect with Creators</p>
+          <p className="description" ref={textRef}></p>
           <button className="cta-button">Get Started →</button>
         </div>
       </div>
